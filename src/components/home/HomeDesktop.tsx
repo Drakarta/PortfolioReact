31f49b60af71fa659d297bdcf61442f336cb5208
Anthony <drakarta3@gmail.com>
2025-04-14 01:21:38 +0200

    intro message start

--- src/components/home/HomeDesktop.tsx
+++ src/components/home/HomeDesktop.tsx
@@ -3,9 +3,10 @@ import HoverIndicator from "./HoverIndicator";
 
 export default function HomeDesktop() {
   return (
-    <div className="absolute top-0 flex h-svh w-full flex-col items-center justify-center">
+    <div className="top-0 flex h-svh w-full flex-col items-center justify-evenly">
       <AntHoney />
       <HoverIndicator />
+      <p className="text-orange-100">Hello I'm Anthony,</p>
     </div>
   );
 }
