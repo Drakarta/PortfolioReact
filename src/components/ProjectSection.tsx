503a395017bf69fa3f3e75469095d0e19b0e2a63
Ant <Drakarta3@gmail.com>
2025-04-25 15:58:29 +0200

    fix

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,13 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
-import { getGithubData } from "./project/github";
+// import { useQuery } from "@tanstack/react-query";
+// import { getGithubData } from "./project/github";
 
 export default function ProjectSection() {
-  const query = useQuery({
-    queryKey: ["github"],
-    queryFn: getGithubData,
-    refetchOnWindowFocus: false,
-    refetchOnReconnect: false,
-  });
+  // const query = useQuery({
+  //   queryKey: ["github"],
+  //   queryFn: getGithubData,
+  //   refetchOnWindowFocus: false,
+  //   refetchOnReconnect: false,
+  // });
+
   return (
     <section id="project" className="z-100 relative w-full h-svh">
       <div></div>
