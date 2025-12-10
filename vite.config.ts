ea9a8da177ed861ca77038e7988b8cfc82e36be3
Anthony <72227148+Drakarta@users.noreply.github.com>
2025-12-10 15:40:06 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import tailwindcss from "@tailwindcss/vite"
+import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    vercel(),
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
   ],
   resolve: {
